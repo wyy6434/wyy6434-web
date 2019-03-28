c0f1ad29d55b639354d9e6e43ad8cbc87303dac0
wyy6434 <46429650+wyy6434@users.noreply.github.com>
2019-03-27 19:55:29 -0700

    3d game with mapbox sdk

---
+++ 3DGame/Assets/Models/Script/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private int xp = 0;
+    [SerializeField] private int requiredXp = 100;
+    [SerializeField] private int levelBase = 100;
+    [SerializeField] private List<GameObject> droids = new List<GameObject>();
+    private int lvl = 1;
+
+    public int Xp {
+        get { return xp; }
+    }
+
+    public int RequiredXp {
+        get { return requiredXp; }
+    }
+
+    public int LevelBase {
+        get { return LevelBase; }
+    }
+    
+    public List<GameObject> Droids { 
+        get { return droids; }
+    }
+
+    private void Start() {
+
+    }
+
+    public void AddXp(int xp) {
+        this.xp = Mathf.Max(0, xp);
+    }
+
+    public void AddDroid(GameObject droid) {
+        droids.Add(droid);
+    }
+
+    private void InitLevelData() {
+        lvl = (xp / levelBase) + 1;
+        requiredXp = levelBase * lvl;
+    }
+}
+
